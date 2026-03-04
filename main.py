f9810c15ee41d4cef9487b22aea49e3cf36f4ec1
zerosugar <git@jrsort.com>
2026-03-03 22:07:56 -0600

    user prompt added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -11,21 +12,27 @@ def api_key():
     client = genai.Client(api_key=api_key)
     return client
 
-def call_to_model(client):
-    hard_coded_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+def call_to_model(client, prompt):
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=hard_coded_prompt
+        model='gemini-2.5-flash', contents=prompt
     )
     if response.usage_metadata == None:
         raise RuntimeError("likely failed API request")
-    print(f"User prompt: {hard_coded_prompt}")
+    print(f"User prompt: {prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: \n{response.text}")
 
+def get_user_input():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    return args.user_prompt
+
 def main():
     client = api_key()
-    call_to_model(client)
+    prompt = get_user_input()
+    call_to_model(client, prompt)
 
 if __name__ == "__main__":
     main()
