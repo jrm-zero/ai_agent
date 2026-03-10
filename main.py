e608cbe212b09f3155d6874d1ae16e7470fc9ea7
zerosugar <git@jrsort.com>
2026-03-09 19:59:30 -0500

    agent is complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 from prompts import system_prompt
 from call_function import available_functions, call_function
 from dotenv import load_dotenv # type: ignore
@@ -17,37 +18,48 @@ def api_key():
 
 def call_to_model(client, prompt):
     messages = [types.Content(role="user", parts=[types.Part(text=prompt.user_prompt)])]
-    response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
-            temperature=0
+    for _ in range(20):
+        response = client.models.generate_content(
+            model='gemini-2.5-flash', 
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+                temperature=0
+            )
         )
-    )
-    if response.usage_metadata == None:
-        raise RuntimeError("likely failed API request")
-    if prompt.verbose == True:
-        print(f"User prompt: {prompt.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if response.function_calls != None:
-        function_responses = []
-        for item in response.function_calls:
-            function_call_result = call_function(item, prompt.verbose)
-            if len(function_call_result.parts) == 0:
-                raise Exception(f"Function {item.name} returned an empty list!")
-            if function_call_result.parts[0].function_response == None:
-                raise Exception(f"Function {item.name} parts[0] returned a None result!")
-            if function_call_result.parts[0].function_response.response == None:
-                raise Exception(f"Function {item.name} returned None as a response!")
-            function_responses.append(function_call_result.parts[0])
-            if prompt.verbose == True:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(f"Response: \n{response.text}")
+        if len(response.candidates) > 0:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+        if response.usage_metadata == None:
+            raise RuntimeError("likely failed API request")
+        if prompt.verbose == True:
+            print(f"User prompt: {prompt.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        if response.function_calls != None:
+            function_responses = []
+            for item in response.function_calls:
+                function_call_result = call_function(item, prompt.verbose)
+                if len(function_call_result.parts) == 0:
+                    raise Exception(f"Function {item.name} returned an empty list!")
+                if function_call_result.parts[0].function_response == None:
+                    raise Exception(f"Function {item.name} parts[0] returned a None result!")
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception(f"Function {item.name} returned None as a response!")
+                function_responses.append(function_call_result.parts[0])
+                if prompt.verbose == True:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+            print(f"Response: \n{response.text}")
+            return
+        
+        messages.append(types.Content(role="user", parts=function_responses))
+    
+    sys.exit("ai agent did not come to a response")
 
 def get_user_input():
     parser = argparse.ArgumentParser(description="Chatbot")
